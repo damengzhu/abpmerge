6e27b66ada90ffc0fbe7e7f21ed525899a84ab0f
damengzhu <80020206+damengzhu@users.noreply.github.com>
2023-08-02 15:21:48 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -8,6 +8,7 @@ curl -o i-4.txt https://raw.githubusercontent.com/damengzhu/abpmerge/main/EasyLi
 curl -o i-5.txt https://filters.adtidy.org/android/filters/20_optimized.txt
 curl -o i-6.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
 curl -o i-7.txt https://filters.adtidy.org/android/filters/17_optimized.txt
+curl -o i-8.txt http://sub.adtchrome.com/adt-chinalist-easylist.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
@@ -22,7 +23,7 @@ num=`cat i-tmp.txt | wc -l`
 # 添加标题和时间
 echo "[Adblock Plus 2.0]" >> i-tpdate.txt
 echo "! Title: ABP Merge Rules" >> i-tpdate.txt
-echo "! Description: 该规则合并自jiekouAD，AdGuard中文语言规则，AdGuard移动横幅广告过滤器，AdGuard URL跟踪过滤器，EasyList no Element Rules，乘风视频广告过滤规则、EasylistChina、EasylistLite、CJX'sAnnoyance，Adblock Warning Removal List以及补充的一些规则" >> i-tpdate.txt
+echo "! Description: 该规则合并自jiekouAD，AdGuard中文语言规则，AdGuard移动横幅广告过滤器，AdGuard URL跟踪过滤器，EasyList no Element Rules，乘风视频广告过滤规则，EasylistChina，ChinaList+EasyList(修正)，EasylistLite，CJX'sAnnoyance，Adblock Warning Removal List以及补充的一些规则" >> i-tpdate.txt
 echo "! Homepage: https://github.com/damengzhu/abpmerge" >> i-tpdate.txt
 echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> i-tpdate.txt
 echo "! Total count: $num" >> i-tpdate.txt
